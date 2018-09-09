4549a35f3dfc424e216c5c2b27abf2145d838bed
wangxianggan <382615666@qq.com>
2018-09-10 00:26:07 +0800

    a

---
+++ js/page/shouey.js
@@ -0,0 +1,13 @@
+$(function () {
+    layui.use('carousel', function(){
+        var carousel = layui.carousel;
+        //建造实例
+        carousel.render({
+            elem: '#carousel',
+            width: '530px',
+            height: '285px',
+            arrow: 'always' //始终显示箭头
+            //anim: 'updown' //切换动画方式
+        });
+    });
+})
