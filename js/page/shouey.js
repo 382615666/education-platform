9348eac815d0e85d5cce293275fc5260067767dd
王湘赣 <wangxianggan@hnjing.com>
2018-09-10 20:34:00 +0800

    a

--- js/page/shouey.js
+++ js/page/shouey.js
@@ -4,7 +4,7 @@ $(function () {
         //建造实例
         carousel.render({
             elem: '#carousel',
-            width: '530px',
+            width: '540px',
             height: '285px',
             arrow: 'always' //始终显示箭头
             //anim: 'updown' //切换动画方式
