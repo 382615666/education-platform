29cb5f9375f30a8320815dddc95e53cad641a5b8
王湘赣 <wangxianggan@hnjing.com>
2018-09-15 16:49:10 +0800

    a

--- js/lib/common.js
+++ js/lib/common.js
@@ -1,5 +1,18 @@
 $(function () {
+    // 关闭详情页
     $('.close-article-detail-btn').on('click', function () {
         window.close()
+    })
+    // 折叠面板扩展
+    $('.layui-colla-item').on('click', function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active')
+        } else {
+            $(this).addClass('active').sibling().removeClass('active')
+        }
+    })
+    // 阻止冒泡
+    $('.layui-colla-item .layui-colla-content-item').on('click', function (e) {
+        e.stopPropagation()
     })
 })
