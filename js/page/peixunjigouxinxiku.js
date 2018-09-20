6000c713c0e58da849b49bdc2842aa84e77a3e69
王湘赣 <wangxianggan@hnjing.com>
2018-09-20 22:20:46 +0800

    a

---
+++ js/page/peixunjigouxinxiku.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#searchInfo').on('click', function () {
+        layui.layer.open({
+            type: 1,
+            title: '职业培训证书',
+            content: 'aa'
+        })
+    })
+
+})
