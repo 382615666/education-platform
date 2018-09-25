9009d4079b1f374ffac5e4b0d0ee305dc7e5ced5
王湘赣 <wangxianggan@hnjing.com>
2018-09-25 18:20:45 +0800

    a

---
+++ js/page/jigongyuanxiaoxinxi.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('.layout-condition-item-box.change').on('click', '.layout-condition-value-item', function () {
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+    $('.layout-condition-item-box').on('click', '.layout-condition-value-item.link.active', function () {
+        $(this).remove()
+    })
+    $('.major-list-wrap').on('click', '.major-item', function () {
+        layer.close(layer.index)
+    })
+    $('#major-btn').on('click', function () {
+        layui.layer.open({
+            type: 1,
+            title: '选择专业',
+            area: '1000px',
+            maxHeight: 700,
+            content: $('#major-change')
+        })
+    })
+})
